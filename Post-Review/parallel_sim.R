adf86b40054062e3bd4d0552fa48517952cc37ef
Andres Perez Alonso <A.F.PerezAlonso@tilburguniversity.edu>
2024-10-18 14:01:17 +0200

    Organize Sim Files
    - Divide the files between pre-review simulation and post-review simulation
    - Clear simulation code of the post-review folder for parallelization

---
+++ Post-Review/parallel_sim.R
@@ -0,0 +1,170 @@
+# Set the working directory
+setwd("~/GitHub/ModelSelection_Simulation/Post-Review")
+
+# Source the relevant functions
+source("MMG-SEM.R")
+source("E_Step.R")
+source("ModelSelection.R")
+source("DataGeneration.R")
+
+# Simulation Design
+# Which factors are going to be tested? For now:
+nclus   <- c(1, 2, 4, 8)   # Number of clusters
+ngroups <- 36              # Number of groups
+coeff   <- c(0.3, 0.4)     # Initial regression parameters
+N_g     <- c(50, 100, 200) # Sample size per groups
+balance <- c("bal", "unb") # Cluster size
+sd      <- c(0, 0.05, 0.1) # Differences within a cluster (in betas)
+
+model <- '
+    # factor loadings
+    F1 =~ x1 + x2 + x3 + x4 + x5
+    F2 =~ z1 + z2 + z3 + z4 + z5
+    F3 =~ m1 + m2 + m3 + m4 + m5
+    F4 =~ y1 + y2 + y3 + y4 + y5
+    
+    # Regression parameters
+    F4 ~ F1 + F3
+    F3 ~ F1 + F2
+'
+S1 <- '
+    # factor loadings
+    F1 =~ x1 + x2 + x3 + x4 + x5
+    F2 =~ z1 + z2 + z3 + z4 + z5
+    F3 =~ m1 + m2 + m3 + m4 + m5
+    F4 =~ y1 + y2 + y3 + y4 + y5
+'
+
+S2 <- '
+    # Regression parameters
+    F4 ~ F1 + F3
+    F3 ~ F1 + F2
+'
+
+# Get design matrix
+design <- expand.grid(nclus, ngroups, coeff, N_g, balance, sd, model) 
+colnames(design) <- c("nclus", "ngroups", "coeff", "N_g", "balance", "sd", "model")
+rownames(design) <- NULL
+rm(balance, coeff, N_g, nclus, sd, ngroups) 
+
+# Functions for the simulation
+# First, to avoid stopping due to errors, create a function with data generation and MMGSEM
+# Errors come from non positive definite cov matrices. This code allows the re-sample
+genDat_analysis <- function(seed, RowDesign, k, NonInv){
+  # Set seed per design condition (row) and replication (K)
+  set.seed(seed)
+  # browser()
+  # Generate data
+  #SimData <- do.call(what = DataGeneration, args = Design[RowDesign, ])$SimData
+  SimData <- DataGeneration(model     = design[RowDesign, "model"], 
+                            nclus     = design[RowDesign, "nclus"], 
+                            ngroups   = design[RowDesign, "ngroups"], 
+                            reg_coeff = design[RowDesign, "coeff"], 
+                            N_g       = design[RowDesign, "N_g"], 
+                            balance   = design[RowDesign, "balance"], 
+                            sd        = design[RowDesign, "sd"])
+  
+  # Save data
+  save(SimData, file = paste("Data/Data", "Row", RowDesign, "Rep", k, ".Rdata", sep = ""))
+  save(seed, file = paste("Seed/Seed", "Row", RowDesign, "Rep", k, ".Rdata", sep = ""))
+  
+  # Run model selection from 1 to 8 clusters
+  # 1. BOTH RES AND LOAD NON-INV ARE INCLUDED
+  results <- ModelSelection(dat = SimData$SimData, 
+                            S1 = S1, 
+                            S2 = S2,
+                            group = "group",
+                            clusters = c(1, 8),
+                            nstarts = 25,
+                            seed = seed, 
+                            group.equal = "loadings",
+                            group.partial = NonInv)
+}
+
+# Create SAFE function (in case of errors)
+genDat_analysis <- purrr::safely(.f = genDat_analysis, otherwise = NULL)
+
+# Main simulation function
+do_sim <- function(RowDesign){
+  
+  # Define non-invariances
+  NonInv <- c("F1 =~ x2", "F1 =~ x3",
+              "F2 =~ z2", "F2 =~ z3",
+              "F3 =~ m2", "F3 =~ m3",
+              "F4 =~ y2", "F4 =~ y3")
+  
+  for(k in 1:K){
+    print(paste("Replication", k, "out of", K))
+    
+    # Code to re-sample in case the covariance matrix is non positive definite
+    attempts <- 10
+    for(j in 1:attempts){
+      # Seed will change if there is an error
+      ctimes <- system.time(
+        results <- genDat_analysis(seed = (RowDesign * k * j), RowDesign = RowDesign, k = k, NonInv = NonInv)
+      ) 
+      test <- results$result
+      if(!is.null(test)){
+        # If there was no error, break the loop and continue
+        break
+      }
+    }
+    
+    # Save computation times
+    save(ctimes, file = paste("Times/Time", "Row", RowDesign, "Rep", k, ".Rdata", sep = ""))
+
+    # Save results if necessary
+    results <- test
+    save(results, file = paste("Fit/Fit", "Row", RowDesign, "Rep", k, "-", j, ".Rdata" , sep = ""))
+    
+  }
+  
+  # Return the final results
+  return(results)
+}
+
+# Set number of replications per condition
+K <- 1 
+
+# save(Results_final, file = "FinalResults.Rdata")
+# save(design, file = "design.Rdata")
+
+# ###################################################################### #
+# ######################## START PARALLELIZATION ####################### #
+# ###################################################################### #
+
+library(foreach)
+library(parallel)
+library(doParallel)
+
+# Get object names
+obj <- objects()
+
+# Setup parallel cluster
+cl <- makeCluster(2)
+registerDoParallel(cl)
+
+# Divide rows per cluster
+# rows_divided <- split(1:2, 1:2)
+
+# Export the necessary function and variables to the cluster
+clusterEvalQ(cl, {
+  library(lavaan)
+  library(MASS)
+  library(combinat)
+})
+clusterEvalQ(cl, setwd("~/GitHub/ModelSelection_Simulation/Post-Review"))
+parallel::clusterExport(cl, varlist = obj)
+
+# Parallel execution over RowDesign
+# results <- parLapply(cl = cl, X = 1:2, fun = do_sim)
+results <- foreach(RowDesign = 1:2) %dopar% {
+  do_sim(RowDesign)
+}
+
+stopCluster(cl)
+
+
+
+
+
